8d19171ad7cadd87075ecac6045f27235f246349
Mikael Freva <mikaelfreva@live.fr>
2022-11-16 17:55:39 +0100

    push

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
+import Router, { useRouter } from 'next/router';
 import '../styles/globals.css'
+import { useEffect, useState } from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
+
+    const [loading, setLoading] = useState(false);
+
+    Router.events.on('routeChangeStart', (url) => {
+      setLoading(true);
+     });
+
+    Router.events.on('routeChangeComplete', (url) => {
+      setLoading(false);
+    });
+   
+
+   return(
+     <>
+       {loading ? (
+          <div className="load"></div>
+         ) : (
+         
+             <Component {...pageProps} />
+         
+         )}
+   </>
+ )
+}
 export default MyApp
